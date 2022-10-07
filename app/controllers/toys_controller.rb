ad1c8a540360e84a51354be2d886944be8c56e92
Okubasufrank <franklineokubasu51@gmail.com>
2022-10-07 14:07:52 +0300

    done

--- app/controllers/toys_controller.rb
+++ app/controllers/toys_controller.rb
@@ -7,19 +7,32 @@ class ToysController < ApplicationController
   end
 
   def create
-    toy = Toys.create(toy_params)
-    render json: toy, status: :created
+    toy = Toy.create(toy_params)
+    if toy
+      render json: toy, status: :updated
+    else
+      error_not_found
+    end
   end
 
   def update
     toy = Toy.find_by(id: params[:id])
-    toy.update(toy_params)
+    if toy
+      toy.update(toy_params)
+      render json: toy
+    else
+      error_not_found
+    end
   end
 
   def destroy
     toy = Toy.find_by(id: params[:id])
-    toy.destroy
-    head :no_content
+    if toy
+      toy.destroy
+      head :no_content
+    else
+      error_not_found
+    end
   end
 
   private
@@ -27,5 +40,10 @@ class ToysController < ApplicationController
   def toy_params
     params.permit(:name, :image, :likes)
   end
+
+  def error_not_found
+    render json: {error: "toy not found"}, status: :not_found
+  end
+  
 
 end
